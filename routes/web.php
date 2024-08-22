b9e7e7427c2a592e83935bf5ae101b15679543b7
warisaB <warisara.sang@kkumail.com>
2024-08-22 17:02:52 +0700

    first

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,24 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WebController;
+use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/myshop', [WebController::class, 'myshop'])->name('web.myshop');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/myshop', [WebController::class,"myshop"])->name("web.myshop");
+
+require __DIR__.'/auth.php';
